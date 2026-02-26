d189392d0d4d47c7c0251709640079578d732541
AtramAniket <aniket_atram@live.in>
2026-02-26 15:49:35 +0530

    added post requests to get cafe by location and adding new cafe

--- app/routes/cafes.py
+++ app/routes/cafes.py
@@ -2,12 +2,13 @@ import json
 from random import randint
 from app.extensions import db
 from app.models.cafe import Cafe
-from flask import Blueprint, jsonify
+from sqlalchemy.exc import SQLAlchemyError
+from flask import Blueprint, jsonify, request
 
 cafe_blueprint = Blueprint('Cafes', __name__)
 
 @cafe_blueprint.route('/all', methods = ['GET'])
-def home():
+def get_all_cafes():
 	all_cafes = db.session.scalars(db.select(Cafe).order_by(Cafe.id)).all() 
 	return jsonify({
 		"status": "success", 
@@ -26,3 +27,67 @@ def get_random_cafe():
 		"status": "success", 
 		"message": "Data is available",
 		"data": random_cafe.to_dict()})
+
+
+@cafe_blueprint.route('/search', methods = ['POST'])
+def get_cafe():
+	incoming_params = request.json
+	cafe_location = incoming_params["location"]
+	print(cafe_location)
+	result = db.session.scalar(db.select(Cafe).where(Cafe.location == cafe_location))
+	if result:
+		return jsonify({
+			"status": "success", 
+			"message": "Data is available",
+			"data": result.to_dict() }), 200
+	else:
+		return jsonify({
+			"status": "Failure", 
+			"message": "Sorry no data available", }), 404
+
+
+@cafe_blueprint.route('/add', methods = ['POST'])
+def add_cafe():
+	incoming_params = request.json
+	name = incoming_params["name"]
+	map_url = incoming_params["map_url"]
+	img_url = incoming_params["img_url"]
+	location = incoming_params["location"]
+	can_take_calls = incoming_params["can_take_calls"]
+	has_toilet = incoming_params["has_toilet"]
+	has_wifi = incoming_params["has_wifi"]
+	has_sockets = incoming_params["has_sockets"]
+	seats = incoming_params["seats"]
+	coffee_price = incoming_params["coffee_price"]
+
+	
+
+	try:
+		
+		new_cafe = Cafe(name=name,map_url=map_url,img_url=img_url,location=location,can_take_calls=can_take_calls,has_toilet=has_toilet,has_wifi=has_wifi,has_sockets=has_sockets,seats=seats,coffee_price=coffee_price)
+		
+		db.session.add(new_cafe)
+		
+		db.session.commit()
+		
+		return jsonify({
+			"status": "Success", 
+			"message": "Data added successfully", }), 200
+	
+	except SQLAlchemyError as e:
+		
+		db.session.rollback()
+		
+		return jsonify({
+			"status": "Failure",
+			"message": "Database eror", 
+			"error": str(e) }), 400
+
+	except Exception as e:
+		
+		db.session.rollback()
+		
+		return jsonify({
+			"status": "Failure", 
+			"message": "Unexpected error", 
+			"error": str(e) }), 400
